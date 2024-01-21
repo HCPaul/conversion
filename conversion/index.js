c10c940dd5983ffcf8d4e7d653381103de6dcff6
HCPaul <paul.hc.1992@gmail.com>
2024-01-20 20:07:26 -0400

    probandoPat

---
+++ conversion/index.js
@@ -0,0 +1,13 @@
+exports.pulgadasaMetros = function (pulgadas) {
+    return pulgadas * 0.0254;
+};
+
+exports.millasaMetros = function(millas){
+    return millas * 1609.34;
+};
+exports.yardasMetros = function(yardas) {
+    return yardas * 0.9144; 
+};
+exports.piesaMetros = function (pies) {
+    return pies *0.3048;
+};
---
+++ inicio.js
@@ -0,0 +1,5 @@
+var conversiones = require("./conversion");
+
+var pulgadas = 3;
+
+console.log( pulgadas + " pulgadas son " + conversiones.pulgadasaMetros(pulgadas) + " metros.");
